800e22e7c9d64a953f21380c8f2a1f34c034083b
mujahidtemp404 <mujahidislam400301@gmail.com>
2024-01-11 12:51:11 +0600

    try to use un reapeted data

--- script.js
+++ script.js
@@ -3,3 +3,93 @@ function toggleMenu() {
     const navList = document.querySelector('.nav-list');
     navList.classList.toggle('show');
 }
+
+
+
+// searvice section script
+
+const cardData = [
+    {
+        title: "Super Easy to Use",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        imageSrc: "img/easy.jpg",
+        iconSrc: "img/icone/easy.svg",
+        learnMoreLink: "#"
+    },
+    {
+        title: "Super Easy to Use",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        imageSrc: "img/easy.jpg",
+        iconSrc: "img/icone/easy.svg",
+        learnMoreLink: "#"
+    },
+    {
+        title: "Super Easy to Use",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        imageSrc: "img/easy.jpg",
+        iconSrc: "img/icone/easy.svg",
+        learnMoreLink: "#"
+    }
+    
+];
+
+
+function createCardSection(data) {
+    const section = document.createElement("div");
+    section.classList.add("cards");
+
+    const cardContent = document.createElement("div");
+    cardContent.classList.add("card-content");
+
+    const icon = document.createElement("aside");
+    const iconImg = document.createElement("img");
+    iconImg.src = data.iconSrc;
+    iconImg.alt = "easy";
+    icon.appendChild(iconImg);
+
+    const title = document.createElement("h2");
+    title.textContent = data.title;
+
+    const paragraph = document.createElement("p");
+    paragraph.textContent = data.content;
+
+    const learnMoreLink = document.createElement("a");
+    learnMoreLink.href = data.learnMoreLink;
+    learnMoreLink.textContent = "Learn More →";
+
+    cardContent.appendChild(icon);
+    cardContent.appendChild(title);
+    cardContent.appendChild(paragraph);
+    cardContent.appendChild(learnMoreLink);
+
+    const cardImage = document.createElement("div");
+    cardImage.classList.add("card-image");
+
+    const image = document.createElement("img");
+    image.src = data.imageSrc;
+    image.alt = "easy";
+
+    cardImage.appendChild(image);
+
+    section.appendChild(cardContent);
+    section.appendChild(cardImage);
+
+    return section;
+}
+
+
+function initializeDynamicSections() {
+    const container = document.getElementById("dynamicSectionsContainer");
+
+    // Loop through the data and create card sections
+    cardData.forEach((data, index) => {
+        const cardSection = createCardSection(data);
+        container.appendChild(cardSection);
+    
+        if (index === 1) {
+            cardSection.classList.add("secondCard");
+        }
+    });
+}
+
+initializeDynamicSections();
