cc12067282ddfe18bc3cf3fe8406f929a30be1c1
mujahidtemp404 <mujahidislam400301@gmail.com>
2024-01-12 23:46:25 +0600

    change some dummy content

--- script.js
+++ script.js
@@ -11,28 +11,28 @@ function toggleMenu() {
 const cardData = [
     {
         title: "Super Easy to Use",
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
+        content: "At Hostify, we believe that managing your hosting should be as easy as 1-2-3. That's why we've designed our platform to provide a super user-friendly experience, making it a breeze for beginners and seasoned users alike.",
         imageSrc: "img/easy.jpg",
         iconSrc: "img/icone/easy.svg",
         learnMoreLink: "#"
     },
     {
         title: "Simply fast websites",
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
+        content: "Experience blazing-fast websites with Hostify. Our powerful infrastructure ensures rapid loading times, enhancing user experience and SEO. Simply fast websites mean more engagement and better performance for your online presence.",
         imageSrc: "img/fast.jpg",
         iconSrc: "img/icone/computer.svg",
         learnMoreLink: "#"
     },
     {
         title: "WordPress made easy",
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
+        content: "Transform your WordPress experience with Hostify's user-friendly platform. Effortlessly manage, customize, and grow your WordPress site. Our intuitive tools and expert support make WordPress easy, so you can focus on creating content that matters",
         imageSrc: "img/wordpress.jpg",
         iconSrc: "img/icone/satting.svg",
         learnMoreLink: "#"
     },
     {
         title: "24/7 Expert Support",
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
+        content: "Count on Hostify for round-the-clock expert support. Our dedicated team is here 24/7 to assist you with any queries or challenges. With Hostify, enjoy the peace of mind that comes with reliable support at all times.",
         imageSrc: "img/support.jpg",
         iconSrc: "img/icone/clock.svg",
         learnMoreLink: "#"
